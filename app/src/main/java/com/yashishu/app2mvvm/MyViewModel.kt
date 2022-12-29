d30393dbf95c4ad0c40422f85d33b152b97ebf00
petrichoraroma <yarasights@gmail.com>
2022-12-29 10:37:58 +0530

    Initial commit

---
+++ app/src/main/java/com/yashishu/app2mvvm/MyViewModel.kt
@@ -0,0 +1,14 @@
+package com.yashishu.app2mvvm
+import androidx.lifecycle.ViewModel
+class MyViewModel:ViewModel() {
+    private var _badge:Int=5
+    val badge:Int
+    get()=_badge
+
+    fun updateBadge(count:Int)
+    {
+        _badge =count
+    }
+
+
+}
